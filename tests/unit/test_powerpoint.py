af4e19e52cdb12f280758bad4dfe59071d13e4e1
Shuhei Nitta <huisintheta@gmail.com>
2023-10-28 05:06:52 +0000

    Update test_powerpoint.py

--- tests/unit/test_powerpoint.py
+++ tests/unit/test_powerpoint.py
@@ -1,46 +1,19 @@
 import os
 import pathlib
-from collections import abc
-from unittest import mock
 
 import dash
 import pytest
-import tlab_analysis.photo_luminescence as pl
+import pytest_mock
+from tlab_analysis import trpl
 
-from dawa_trpl import data_system as ds
 from dawa_trpl import powerpoint
 from tests import IMGDIR, FixtureRequest
 
 
 @pytest.fixture(scope="module", params=[path.name for path in IMGDIR.glob("*.img")])
-def pldata(request: FixtureRequest[str]) -> pl.Data:
+def trpl_data(request: FixtureRequest[str]) -> trpl.TRPLData:
     filepath = IMGDIR / request.param
-    return pl.read_img(filepath)
-
-
-@pytest.fixture(scope="module")
-def time_resolved(pldata: pl.Data) -> pl.TimeResolved:
-    return pldata.resolve_along_time()
-
-
-@pytest.fixture(scope="module")
-def wavelength_resolved(pldata: pl.Data) -> pl.WavelengthResolved:
-    wr = pldata.resolve_along_wavelength()
-    return ds._fit_to_wavelength_resolved(wr)
-
-
-@pytest.fixture()
-def ds_mock(
-    pldata: pl.Data,
-    time_resolved: pl.TimeResolved,
-    wavelength_resolved: pl.WavelengthResolved,
-) -> abc.Generator[mock.Mock, None, None]:
-    with mock.patch("dawa_trpl.powerpoint.ds") as m:
-        m.get_existing_item_filepaths.return_value = ["item.img"]
-        m.load_pldata.return_value = pldata
-        m.load_time_resolved.return_value = time_resolved
-        m.load_wavelength_resolved.return_value = wavelength_resolved
-        yield m
+    return trpl.read_file(filepath)
 
 
 @pytest.mark.parametrize("selected_items", [list(), None])
@@ -67,13 +40,19 @@ def test_update_download_button_ability_when_multiple_items_are_selected(
 
 
 @pytest.mark.parametrize("selected_items", [["item.img"]])
-@pytest.mark.parametrize("wavelength_range", [[400, 500]])
+@pytest.mark.parametrize("wavelength_range", [[460, 480]])
 def test_download_powerpoint_when_items_are_selected(
-    ds_mock: mock.Mock,
     selected_items: list[str],
     upload_dir: str,
     wavelength_range: list[int],
-) -> None:  # TODO: Refactor me
+    trpl_data: trpl.TRPLData,
+    mocker: pytest_mock.MockerFixture,
+) -> None:
+    get_existing_item_filepaths_mock = mocker.patch(
+        "dawa_trpl.data_system.get_existing_item_filepaths", return_value=["item.img"]
+    )
+    mocker.patch("dawa_trpl.data_system.load_trpl_data", return_value=trpl_data)
+    validate_upload_dir_mock = mocker.patch("dawa_trpl.data_system.validate_upload_dir")
     result = powerpoint.download_powerpoint(
         n_clicks=1,
         selected_items=selected_items,
@@ -90,18 +69,10 @@ def test_download_powerpoint_when_items_are_selected(
     assert result["base64"] is True
     # TODO: How to assert `content`?
     assert set(result.keys()) == {"filename", "content", "type", "base64"}
-    ds_mock.validate_upload_dir.assert_called_once_with(upload_dir)
-    ds_mock.get_existing_item_filepaths.assert_called_once_with(
+    validate_upload_dir_mock.assert_called_once_with(upload_dir)
+    get_existing_item_filepaths_mock.assert_called_once_with(
         selected_items,
-        ds_mock.validate_upload_dir.return_value,
-    )
-    ds_mock.load_time_resolved.assert_called_once_with(
-        ds_mock.get_existing_item_filepaths.return_value[0]
-    )
-    ds_mock.load_wavelength_resolved.assert_called_once_with(
-        ds_mock.get_existing_item_filepaths.return_value[0],
-        tuple(wavelength_range),
-        fitting=True,
+        validate_upload_dir_mock.return_value,
     )
 
 
