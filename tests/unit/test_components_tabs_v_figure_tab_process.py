1a4758ff98370f71e77ee2736ddeb4f23878bef3
Shuhei Nitta <huisintheta@gmail.com>
2023-10-28 14:04:24 +0000

    Update test_components_tabs_v_figure_tab_process.py

--- tests/unit/test_components_tabs_v_figure_tab_process.py
+++ tests/unit/test_components_tabs_v_figure_tab_process.py
@@ -1,8 +1,8 @@
 import pathlib
 
+import pandas as pd
 import plotly.graph_objects as go
 import pytest
-import tlab_analysis.photo_luminescence as pl
 
 from dawa_trpl import data_system as ds
 from dawa_trpl.components.tabs.v_figure_tab import process
@@ -10,20 +10,20 @@ from tests import IMGDIR
 
 
 @pytest.fixture(scope="module")
-def wrs() -> list[pl.WavelengthResolved]:
-    def load_wavelength_resolved(filepath: pathlib.Path) -> pl.WavelengthResolved:
-        wr = ds.load_wavelength_resolved(filepath.as_posix())
-        return wr
+def dfs() -> list[pd.DataFrame]:
+    def load_time_df(filepath: pathlib.Path) -> pd.DataFrame:
+        df = ds.load_time_df(filepath.as_posix())
+        return df
 
-    return list(map(load_wavelength_resolved, IMGDIR.glob("*.img")))
+    return list(map(load_time_df, IMGDIR.glob("*.img")))
 
 
 @pytest.mark.parametrize("log_y", [True, False])
-def test_create_figure(wrs: list[pl.WavelengthResolved], log_y: bool) -> None:
-    fig = process.create_figure(wrs, log_y)
+def test_create_figure(dfs: list[pd.DataFrame], log_y: bool) -> None:
+    fig = process.create_figure(dfs, log_y)
     assert isinstance(fig, go.Figure)
 
 
-def test_add_fitting_curve(wrs: list[pl.WavelengthResolved]) -> None:
-    fig = process.add_fitting_curve(go.Figure(), wrs)
+def test_add_fitting_curve(dfs: list[pd.DataFrame]) -> None:
+    fig = process.add_fitting_curve(go.Figure(), dfs)
     assert isinstance(fig, go.Figure)
